aa07db8ff62262ecf465e8136d674889ec0cfda9
王小泽 <wangze@anbangke.com>
2023-05-23 17:31:55 +0800

    fix: 修复路由加载模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import cssRoute from "./module/css";
 import jsRoute from "./module/javascript";
 import vueRoute from "./module/vue";
 
 export const baseRoutes = [...cssRoute, ...jsRoute, ...vueRoute];
 export const baseRouter = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
