656c5c610b2203e75bc94c4e3c5aad13049a406b
王小泽 <wangxiaoze@petalmail.com>
2024-11-29 23:25:58 +0800

    feat: 添加动画

--- static/javascript/config.js
+++ static/javascript/config.js
@@ -11,8 +11,44 @@ export const navs = {
     icon: "ri-pencil-ruler-2-line",
     path: "irregular_shapes",
   },
-  // 图片调色盘: {
-  //   icon: "ri-palette-line",
-  //   path: "image_palette",
-  // },
+  图片调色盘: {
+    icon: "ri-palette-line",
+    path: "image_palette",
+  },
+  抽牌: {
+    icon: "ri-refund-line",
+    path: "draw_cards",
+  },
+  图片缩放: {
+    icon: "ri-landscape-ai-line",
+    path: "zoom_zither",
+  },
+  文字背景裁剪: {
+    icon: "ri-text",
+    path: "text_background_cropping",
+  },
+  文字交融: {
+    icon: "ri-text",
+    path: "text_fusion",
+  },
+  翻书: {
+    icon: "ri-git-repository-line",
+    path: "novel_3D",
+  },
+  取色器: {
+    icon: "ri-dropper-line",
+    path: "color_picker",
+  },
+  掉落小块: {
+    icon: "ri-pie-chart-box-line",
+    path: "small_pieces_falling",
+  },
+  小球随着鼠标移动: {
+    icon: "ri-scroll-to-bottom-line",
+    path: "mouse_ball",
+  },
+  跳动小球: {
+    icon: "ri-golf-ball-line",
+    path: "bouncing_ball",
+  },
 };
