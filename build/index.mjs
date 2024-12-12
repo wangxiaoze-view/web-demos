a3ee3b69c3fbc5faf0ee481b087d0038e4000af7
王小泽 <wangxiaoze@petalmail.com>
2024-12-12 17:38:13 +0800

    test

--- build/index.mjs
+++ build/index.mjs
@@ -1,60 +1,60 @@
-import { writeFileSync } from "node:fs";
+// import { writeFileSync } from "node:fs";
 
-const vercelJson = {
-  rewrites: [
-    {
-      source: "/web-demos/:match*",
-      destination: "/index.html",
-    },
-  ],
-  heders: [
-    {
-      source: "/assets/(.*)",
-      headers: [
-        {
-          key: "Cache-Control",
-          value: "max-age=31536000, immutable",
-        },
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "*",
-        },
-      ],
-    },
-    {
-      source: "/examples/(.*)",
-      headers: [
-        {
-          key: "Cache-Control",
-          value: "max-age=31536000, immutable",
-        },
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "*",
-        },
-      ],
-    },
-    {
-      source: "/js/(.*)",
-      headers: [
-        {
-          key: "Cache-Control",
-          value: "max-age=31536000, immutable",
-        },
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "*",
-        },
-      ],
-    },
-  ],
-  github: {
-    silent: true,
-  },
-};
+// const vercelJson = {
+//   rewrites: [
+//     {
+//       source: "/web-demos/:match*",
+//       destination: "/index.html",
+//     },
+//   ],
+//   heders: [
+//     {
+//       source: "/assets/(.*)",
+//       headers: [
+//         {
+//           key: "Cache-Control",
+//           value: "max-age=31536000, immutable",
+//         },
+//         {
+//           key: "Access-Control-Allow-Origin",
+//           value: "*",
+//         },
+//       ],
+//     },
+//     {
+//       source: "/examples/(.*)",
+//       headers: [
+//         {
+//           key: "Cache-Control",
+//           value: "max-age=31536000, immutable",
+//         },
+//         {
+//           key: "Access-Control-Allow-Origin",
+//           value: "*",
+//         },
+//       ],
+//     },
+//     {
+//       source: "/js/(.*)",
+//       headers: [
+//         {
+//           key: "Cache-Control",
+//           value: "max-age=31536000, immutable",
+//         },
+//         {
+//           key: "Access-Control-Allow-Origin",
+//           value: "*",
+//         },
+//       ],
+//     },
+//   ],
+//   github: {
+//     silent: true,
+//   },
+// };
 
-try {
-  writeFileSync("dist/vercel.json", JSON.stringify(vercelJson, null, 2));
-} finally {
-  process.exit(0);
-}
+// try {
+//   writeFileSync("dist/vercel.json", JSON.stringify(vercelJson, null, 2));
+// } finally {
+//   process.exit(0);
+// }
